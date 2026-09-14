09fab4adf867d54667d371f9784d33a59e5fe3fe
jleonm506-create <jleonm506@gmail.com>
2026-09-14 16:27:40 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Datos del proyecto de Supabase de Andrés.
+// La publishable key es pública por diseño; lo que protege los datos es el RLS de la tabla `estado`.
+window.SOBRES_CONFIG = {
+  url: 'https://cnqqwzwwisawnemogbop.supabase.co',
+  anonKey: 'sb_publishable_joeDb4WwPwRwT2dYQAwPAA_f3m06MLd',
+};
